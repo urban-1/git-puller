8af5c712bcb82112624c738d7baf514b1f2f0c93
andreas.bontozoglou <andreas.bontozoglou@sky.uk>
2016-08-02 12:31:12 +0100

    Post-script cron check and install example

--- test/sample-post-puller.sh
+++ test/sample-post-puller.sh
@@ -4,5 +4,8 @@ echo "########################################"
 echo "# ALL UPDATED, NOW DOING SMART STUFF   #"
 echo "########################################"
 
+# Check and install crontab entries
+# crontab -l | grep 'python /mount/share/script.py' || \
+#   (crontab -l && echo '*/1 * * * * python /mount/share/script.py') | crontab - 
 
 exit 0
